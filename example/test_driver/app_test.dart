b8dc78d3aa3075f829352b89aaa598a39b28b5f6
Iqbal <iqbalmineraltown@gmail.com>
2019-05-23 09:58:38 +0700

    add flavor example

--- example/test_driver/app_test.dart
+++ example/test_driver/app_test.dart
@@ -20,7 +20,7 @@ Future<void> main() {
     ..customStepParameterDefinitions = [ColourParameter()]
     ..restartAppBetweenScenarios = true
     ..targetAppPath = "test_driver/app.dart"
-    // ..buildFlavor = "staging" // uncomment when using build flavor
+    // ..buildFlavor = "staging" // uncomment when using build flavor and check android/ios flavor setup
     // ..tagExpression = "@smoke" // uncomment to see an example of running scenarios based on tag expressions
     ..exitAfterTestRun = true; // set to false if debugging to exit cleanly
   return GherkinRunner().execute(config);
