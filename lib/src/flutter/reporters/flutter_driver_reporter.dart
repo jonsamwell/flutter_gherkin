26e1bfa258bf0416b715c30cc889f9d42ae67a0a
Jon Samwell <jonsamwell@gmail.com>
2019-09-24 09:58:36 +1000

    feat(reporters): updated flutter driver reporter to have more fined grained control of what is logged

--- lib/src/flutter/reporters/flutter_driver_reporter.dart
+++ lib/src/flutter/reporters/flutter_driver_reporter.dart
@@ -12,17 +12,42 @@ import 'package:flutter_driver/flutter_driver.dart';
 /// stderr stream.  So Flutter driver will log a normal info message to the stderr and thus make
 /// the process fail from the perspective of a CI server.
 class FlutterDriverReporter extends Reporter {
-  List<StreamSubscription> subscriptions = List<StreamSubscription>();
+  final bool logErrorMessages;
+  final bool logWarningMessages;
+  final bool logInfoMessages;
+  final List<StreamSubscription> subscriptions = List<StreamSubscription>();
+
+  FlutterDriverReporter({
+    this.logErrorMessages = true,
+    this.logWarningMessages = true,
+    this.logInfoMessages = false,
+  });
 
   Future<void> onTestRunStarted() async {
-    subscriptions.add(
-        flutterDriverLog.where((log) => !isErrorLog(log.level)).listen((log) {
-      stdout.writeln(log.toString());
-    }));
-    subscriptions.add(
-        flutterDriverLog.where((log) => isErrorLog(log.level)).listen((log) {
-      stderr.writeln(log.toString());
-    }));
+    if (logInfoMessages) {
+      subscriptions.add(flutterDriverLog
+          .where((log) => _isLevel(log.level, [LogLevel.info, LogLevel.trace]))
+          .listen((log) {
+        stdout.writeln(log.toString());
+      }));
+    }
+
+    if (logWarningMessages) {
+      subscriptions.add(flutterDriverLog
+          .where((log) => _isLevel(log.level, [LogLevel.warning]))
+          .listen((log) {
+        stdout.writeln(log.toString());
+      }));
+    }
+
+    if (logErrorMessages) {
+      subscriptions.add(flutterDriverLog
+          .where(
+              (log) => _isLevel(log.level, [LogLevel.critical, LogLevel.error]))
+          .listen((log) {
+        stderr.writeln(log.toString());
+      }));
+    }
   }
 
   Future<void> dispose() async {
@@ -30,6 +55,6 @@ class FlutterDriverReporter extends Reporter {
     subscriptions.clear();
   }
 
-  bool isErrorLog(LogLevel level) =>
-      level == LogLevel.error || level == LogLevel.critical;
+  bool _isLevel(LogLevel level, Iterable<LogLevel> levels) =>
+      levels.contains(level);
 }
