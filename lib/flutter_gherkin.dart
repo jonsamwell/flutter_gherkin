e4383b465848bf95ddd4c2e66a3303870567adf3
Tim Shedor <tshedor@greenbits.com>
2020-07-31 11:20:38 -0700

    export steps

--- lib/flutter_gherkin.dart
+++ lib/flutter_gherkin.dart
@@ -12,6 +12,17 @@ export 'src/flutter/steps/when_fill_field_step.dart';
 export 'src/flutter/steps/when_pause_step.dart';
 export 'src/flutter/steps/when_tap_widget_step.dart';
 export 'src/flutter/steps/restart_app_step.dart';
+export 'src/flutter/steps/sibling_contains_text_step.dart';
+export 'src/flutter/steps/swipe_step.dart';
+export 'src/flutter/steps/tap_text_within_widget_step.dart';
+export 'src/flutter/steps/tap_widget_of_type_step.dart';
+export 'src/flutter/steps/tap_widget_of_type_within_step.dart';
+export 'src/flutter/steps/tap_widget_with_text_step.dart';
+export 'src/flutter/steps/text_exists_step.dart';
+export 'src/flutter/steps/text_exists_within_step.dart';
+export 'src/flutter/steps/wait_until_key_exists_step.dart';
+export 'src/flutter/steps/when_tap_the_back_button_step.dart';
+export 'src/flutter/steps/wait_until_type_exists_step.dart';
 
 // Hooks
 export 'src/flutter/hooks/attach_screenshot_on_failed_step_hook.dart';
