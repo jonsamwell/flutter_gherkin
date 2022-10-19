d36f74f3c59d6b56f43f18685cdce0a0979056e3
Lin, Chen (External) <Chen.Lin@dentsplysirona.com>
2022-10-19 11:01:15 +0800

    use step name as screenshot name

--- lib/src/flutter/hooks/attach_screenshot_on_failed_step_hook.dart
+++ lib/src/flutter/hooks/attach_screenshot_on_failed_step_hook.dart
@@ -14,7 +14,7 @@ class AttachScreenshotOnFailedStepHook extends Hook {
         stepResult.result == StepExecutionResult.error ||
         stepResult.result == StepExecutionResult.timeout) {
       try {
-        final screenshotData = await _takeScreenshot(world);
+        final screenshotData = await _takeScreenshot(world, step);
         world.attach(screenshotData, 'image/png', step);
       } catch (e, st) {
         world.attach('Failed to take screenshot\n$e\n$st', 'text/plain', step);
@@ -22,8 +22,10 @@ class AttachScreenshotOnFailedStepHook extends Hook {
     }
   }
 
-  Future<String> _takeScreenshot(World world) async {
-    final bytes = await (world as FlutterWorld).appDriver.screenshot();
+  Future<String> _takeScreenshot(World world, String step) async {
+    final bytes = await (world as FlutterWorld)
+        .appDriver
+        .screenshot(screenshotName: step);
 
     return base64Encode(bytes);
   }
